27c00b94b38a3714c6bc1e0483bc66032299f6d3
onracr <onracr_ms87@hotmail.com>
2019-03-20 23:56:52 +0300

    improvements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
@@ -12,13 +14,12 @@ class Program
     static void Main(string[] args)
     {
         Game(array2D);
-
     }
 
     public static void PrintMatrix(string[,] refArray)
     {
         Console.Clear();
-        Console.WriteLine(Environment.NewLine + 
+        Console.WriteLine(Environment.NewLine +
         "---------------------Tic Tac Toe Game---------------------" +
         Environment.NewLine);
 
@@ -32,34 +33,28 @@ class Program
         Console.Write(Environment.NewLine);
     }
 
-    public static void Game(string[,] array2D)
+    public static void Game(string[,] refArray)
     {
-        int tempX = 0;
-        int tempY = 0;
-        int counter = 0;
-        bool condition = true;
 
-        while (condition) {
-            PrintMatrix(array2D);
+        while (true) {
+            PrintMatrix(refArray);
 
-            if (counter == 3) {
-                if (WinCondition(array2D))
-                    Console.WriteLine("You Win!");
-                else {
-                    Console.WriteLine("Nobody Wins!");
+
+            if (WinCondition(refArray)) {
+                Console.WriteLine("You Win!");
+                Console.Write("Play Again? (y/n)  ");
+                char input = Convert.ToChar(Console.ReadLine());
+                if (input == 'y') {
+                    ResetGame();
                 }
-                condition = false;
+                else{
+                    Console.WriteLine("Good Bye!");
+                    break;
+                }
             }
 
-            Console.Write(@"Where do you want to put your X : ");
-            tempX = int.Parse(Console.ReadLine());
-            tempY = int.Parse(Console.ReadLine());
-            array2D[tempX, tempY] = "X";
-            counter++;
-
-            if (tempX == -1 || tempY == -1) break;
-            
-            AiController(array2D);
+            PlayerTurn(refArray);
+            AiTurn(refArray);
         }
     }
 
@@ -80,15 +75,30 @@ class Program
             }
         }
 
-        if (array2D[0, 0] == array2D[1, 1] && array2D[0, 0] == array2D[2, 2])
+        if (array2D[0, 0] == array2D[1, 1] && array2D[0, 0] == array2D[2, 2] && array2D[0, 0] != "_")
             return true;
-        else if (array2D[2, 0] == array2D[1, 1] && array2D[2, 0] == array2D[0, 2])
+        else if (array2D[2, 0] == array2D[1, 1] && array2D[2, 0] == array2D[0, 2] && array2D[1, 1] != "_")
             return true;
 
         return false;
     }
 
-    public static void AiController(string[,] array2D)
+    public static void ResetGame()
+    {
+        string[,] originalArray = new[,]{
+            { "_", "_", "_"},
+            { "_", "_", "_"},
+            { "_", "_", "_"},
+        };
+
+        for (int i = 0; i < originalArray.GetLength(0); i++) {
+            for (int j = 0; j < originalArray.GetLength(1); j++)
+                array2D[i, j] = originalArray[i, j];
+        }
+        PrintMatrix(array2D);
+    }
+
+    public static void AiTurn(string[,] array2D)
     {
         bool conditionMet = false;
 
@@ -97,10 +107,36 @@ class Program
             int aiTurnX = rand.Next(0, 3);
             int aiTurnY = rand.Next(0, 3);
 
-            if (array2D[aiTurnX, aiTurnY] != "X") {
+            if (array2D[aiTurnX, aiTurnY] != "X" && array2D[aiTurnX, aiTurnY] != "O") {
                 array2D[aiTurnX, aiTurnY] = "O";
                 conditionMet = true;
             }
         }
+        Console.WriteLine("AI's turn...");
+        System.Threading.Thread.Sleep(1000);
     }
+
+    public static void PlayerTurn(string[,] refArray)
+    {
+        int tempX = 0;
+        int tempY = 0;
+        bool condition = false;
+
+        while (!condition) {
+            Console.Write("Please enter the X and Y coordiantes, respectively : ");
+            tempX = int.Parse(Console.ReadLine());
+            tempY = int.Parse(Console.ReadLine());
+
+            if (refArray[tempX, tempY] != "O" && refArray[tempX, tempY] != "X") {
+                refArray[tempX, tempY] = "X";
+                condition = true;
+            }
+            else
+                Console.WriteLine("Those coordinates are taken, Please choose some other ones.");
+            
+
+        }
+        PrintMatrix(refArray);
+    }
+
 }
